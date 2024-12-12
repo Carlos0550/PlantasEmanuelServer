9128fbc68ce45ea6f3668ca28b3f11364048dd76
carlos0550 <carlospelinski03@gmail.com>
2024-12-12 16:05:59 -0300

    Implementa nueva logica para editar los productos y sus imagenes y eliminado de productos

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,6 +1,7 @@
 const { pool } = require("../config/database.js");
 
 const saveProduct = async(req,res)=> {
+    console.log("Me ejecuto en save product")
     const {  product_name, product_description, product_price, product_category } = req.body;
     const images = req.files
 
@@ -80,9 +81,9 @@ const getProducts = async(req,res) => {
             }
 
             if (Buffer.isBuffer(product.image_data)) {
-                console.log('Image size in Buffer:', product.image_data.length);
+
                 const imageBase64 = product.image_data.toString("base64");
-                console.log(imageBase64.length)
+
                 acc[product.id].images.push({
                     image_name: product.image_name,
                     image_type: product.image_type,
@@ -105,80 +106,115 @@ const getProducts = async(req,res) => {
     }
 };
 
-const updateProduct = async(req,res)=> {
-    const {  product_name, product_description, product_price, product_category } = req.body;
-    const images = req.files
-    const { product_id } = req.params
-console.log(product_id)
-    if(!product_id || !product_name || !product_description || !product_price) 
-        return res.status(400).json({ msg: "El servidor no recibió algunos datos necesarios" })
-    
-    if (!images || images.length === 0) {
-        return res.status(400).json({ msg: "No se recibieron imágenes para el producto" })
-    }
+const updateProduct = async (req, res) => {
+    const { product_name, product_description, product_price, product_category, imagesWithEdit } = req.body;
+    const images = req.files; 
+    const { product_id } = req.params;
 
-    const query1 =  `
-        UPDATE products SET product_name = $1, product_description = $2, product_category = $3, product_price = $4 WHERE id = $5
-    `
-    const query2 = `
-        DELETE FROM product_images WHERE product_id = $1
-    `
-
-    const query3 = `
-            INSERT INTO product_images (product_id, image_name, image_type, image_size, image_data)
-            VALUES ($1, $2, $3, $4, $5);
-    `;
+    if (!product_id) return res.status(400).json({ msg: "Faltan datos importantes" });
 
     let client;
     try {
-        client = await pool.connect()
+        client = await pool.connect();
         await client.query("BEGIN")
+        const imagesList = await client.query(`SELECT image_name FROM product_images WHERE product_id = $1;`, [product_id]);
+        const existentImages = imagesList.rows.map(row => row.image_name);
 
-        const response = await client.query(query1, [product_name, product_description, product_category, product_price, product_id])
+        const imagesWithEditParsed = JSON.parse(imagesWithEdit);
+        const imagesToKeep = imagesWithEditParsed.map(image => image.editing === true ? image.image_name : "").filter(image => image !== "");
 
-        if(response.rowCount === 0){
-            await client.query("ROLLBACK")
-            return res.status(400).json({ msg: "No se pudo actualizar el producto" })
+        const imagesToDelete = existentImages.filter(img => !imagesToKeep.includes(img));
+
+        const newImages = images.filter(img => !imagesWithEditParsed.some(i => i.image_name === img.originalname && i.editing));
+
+        if(imagesToDelete.length > 0){
+            for (const imageName of imagesToDelete) {
+                const resultDelete = await client.query(`DELETE FROM product_images WHERE image_name = ANY($1) AND product_id = $2;`, [imageName, product_id]);
+                if(resultDelete.rowCount === 0){
+                    await client.query("ROLLBACK")
+                    return res.status(400).json({msg: "Ocurrió un error inesperado y no se pudo actualizar el producto."})
+                }
+            }
+        }
+
+        if(newImages.length > 0){
+            const insertQueries = newImages.map(image => client.query(`
+                INSERT INTO product_images (product_id, image_name, image_data, image_type, image_size) 
+                VALUES ($1, $2, $3, $4, $5);`, 
+                [product_id, image.originalname, image.buffer, image.mimetype, image.size]
+            ));
+            const resultInsertImage = await Promise.all(insertQueries);
+            const someInsertFalied = resultInsertImage.some(result => result.rowCount === 0);
+            
+            if(someInsertFalied){
+                await client.query("ROLLBACK")
+                return res.status(400).json({msg: "Ocurrió un error inesperado y no se pudo actualizar el producto."})
+            }
         }
 
         
-        const imageDeleteResponse = await client.query(query2, [product_id])
-        if(imageDeleteResponse.rowCount === 0){
+
+        const resultUpdateProduct = await client.query(`
+            UPDATE products SET 
+                product_name = $1, 
+                product_description = $2, 
+                product_price = $3, 
+                product_category = $4 
+            WHERE id = $5;`, 
+            [product_name, product_description, product_price, product_category, product_id]
+        );
+
+        if(resultUpdateProduct.rowCount === 0){
             await client.query("ROLLBACK")
-            return res.status(400).json({ msg: "No se pudieron eliminar las imágenes anteriores" })
-        }
-
-        const imageInsertPromises = images.map(image => {
-            const imageValues = [
-                product_id,
-                image.originalname,
-                image.mimetype,
-                image.size,
-                image.buffer
-            ];
-
-            return client.query(query3, imageValues);
-        });
-
-        const imageResponses = await Promise.all(imageInsertPromises);
-        console.log(imageResponses)
-        const totalInsertedImages = imageResponses.reduce((acc, imgRes) => acc + imgRes.rowCount, 0);
-        if (totalInsertedImages !== images.length) {
-            await client.query("ROLLBACK");
-            return res.status(400).json({ msg: "No se pudieron registrar todas las imágenes" });
+            return res.status(400).json({msg: "Ocurrió un error inesperado y no se pudo actualizar el producto."})
         }
 
         await client.query("COMMIT")
-        return res.status(200).json({ msg: "Producto actualizado con éxito" })
+        res.status(200).json({ msg: "Producto actualizado con éxito" });
+
     } catch (error) {
         console.log(error)
         await client.query("ROLLBACK")
-        return res.status(500).json({ msg: "Error interno del servidor al registrar el producto" })
-    }finally{
-        if(client) client.release()
+        res.status(500).json({ msg: "Error en el servidor", error });
+    } finally {
+        if (client) client.release();
+    }
+};
+
+const deleteProduct = async(req,res) => {
+    const { product_id } = req.params;
+
+    if (!product_id) return res.status(400).json({ msg: "Faltan datos importantes" });
+
+    let client;
+    try {
+        client = await pool.connect();
+        await client.query("BEGIN");
+        const result1 = await client.query(`DELETE FROM product_images WHERE product_id = $1;`, [product_id]);
+
+        if (result1.rowCount === 0){
+            await client.query("ROLLBACK")
+            return res.status(400).json({ msg: "No se pudo eliminar el producto" });
+        }
+
+        const result2 = await client.query(`DELETE FROM products WHERE id = $1;`, [product_id]);
+
+        if (result2.rowCount === 0){
+            await client.query("ROLLBACK")
+            return res.status(400).json({ msg: "No se pudo eliminar el producto" });
+        }
+
+        await client.query("COMMIT")
+        res.status(200).json({ msg: "Producto eliminado con exito" });
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ msg: "Error interno del servidor al eliminar el producto" });
+    } finally {
+        if (client) client.release();
     }
 }
 
+
 module.exports = {
-    saveProduct, getProducts, updateProduct
+    saveProduct, getProducts, updateProduct, deleteProduct
 }
