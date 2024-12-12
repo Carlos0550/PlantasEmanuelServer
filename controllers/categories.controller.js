2fdecba1efcbb981363ddc8e242063e52c3efc9f
carlos0550 <carlospelinski03@gmail.com>
2024-12-12 19:01:37 -0300

    Agrega editado de categorias

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -2,7 +2,6 @@ const { pool } = require("../config/database.js");
 
 const saveCategories = async(req,res) => {
     const { category_name } = req.body
-    console.log(req.body)
 
     if(!category_name) return res.status(400).json({ msg: "Por favor ingrese el nombre de la categoría" })
         const query1 = `INSERT INTO categories(category_name) VALUES($1)`
@@ -23,7 +22,7 @@ const saveCategories = async(req,res) => {
 }
 
 const getCategories = async(req,res) => {
-    const query = `SELECT * FROM categories`
+    const query = `SELECT * FROM categories ORDER BY id ASC`
     let client;
     try {
         client = await pool.connect()
@@ -38,4 +37,27 @@ const getCategories = async(req,res) => {
     }
 }
 
-module.exports = { saveCategories, getCategories }
+const editCategory = async(req,res) => {
+    const { category_name } = req.body
+    const { category_id } = req.params
+    console.log(category_name,category_id)
+    if(!category_name || !category_id) return res.status(400).json({ msg: "El servidor no pudo editar la categoría seleccionada. Por favor intente nuevamente" })
+
+    const query1 = `UPDATE categories SET category_name = $1 WHERE id = $2`
+
+    let client;
+    try {
+        client = await pool.connect()
+
+        const result = await client.query(query1, [category_name, category_id])
+        if(result.rowCount === 0) return res.status(400).json({ msg: "No se pudo actualizar la categoría" })
+        return res.status(200).json({ msg: "Categoría actualizada con exito" })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ msg: "Error interno del servidor al actualizar la categoría" })
+    }finally{
+        if(client) client.release()
+    }
+}
+
+module.exports = { saveCategories, getCategories, editCategory }
--- routes/categories.routes.js
+++ routes/categories.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router()
 
-const { getCategories, saveCategories } = require("../controllers/categories.controller.js")
+const { getCategories, saveCategories, editCategory } = require("../controllers/categories.controller.js")
 
 router.get("/get-categories", getCategories)
 router.post("/save-category", saveCategories)
+router.put("/edit-category/:category_id", editCategory)
 
 module.exports = router
