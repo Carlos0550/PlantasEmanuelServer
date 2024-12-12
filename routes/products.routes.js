a770b15d544c241137de775ae4dc27f4d34b569e
carlos0550 <carlospelinski03@gmail.com>
2024-12-12 16:06:11 -0300

    Implementa ruta para eliminado de productos

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const multer = require("multer");
-const { saveProduct, getProducts, updateProduct } = require("../controllers/products.controller");
+const { saveProduct, getProducts, updateProduct, deleteProduct } = require("../controllers/products.controller");
 
 const storage = multer.memoryStorage()
 const upload = multer({storage})
@@ -10,5 +10,6 @@ const upload = multer({storage})
 router.post("/save-product", upload.array("images"), saveProduct);
 router.get("/get-products", getProducts)
 router.put("/edit-product/:product_id", upload.array("images"), updateProduct)
+router.delete("/delete-product/:product_id", deleteProduct)
 
 module.exports = router
