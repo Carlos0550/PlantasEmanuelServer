7a9af7aa900a3bb64e108b66b0e1c35be23bf6c6
carlos0550 <carlospelinski03@gmail.com>
2024-12-16 19:59:38 -0300

    Ruta para eliminado de promociones

--- routes/promotions.routes.js
+++ routes/promotions.routes.js
@@ -4,9 +4,10 @@ const multer = require("multer")
 const storage = multer.memoryStorage()
 const upload = multer({storage})
 
-const { savePromotion, getPromotions } = require("../controllers/promotions.controller.js")
+const { savePromotion, getPromotions, deletePromotion } = require("../controllers/promotions.controller.js")
 
 router.post("/save-promotion", upload.array("promotion_images"), savePromotion)
 router.get("/get-promotions", getPromotions)
+router.delete("/delete-promotion/:promotion_id", deletePromotion)
 
 module.exports = router
