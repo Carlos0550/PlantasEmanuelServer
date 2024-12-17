f446dce4302229f2fc06daecf62aee1451c4569a
carlos0550 <carlospelinski03@gmail.com>
2024-12-17 19:05:06 -0300

    Agrega editado de promociones

--- controllers/promotions.controller.js
+++ controllers/promotions.controller.js
@@ -1,7 +1,6 @@
 const { pool } = require("../config/database.js")
 
 const savePromotion = async(req,res) => {
-    console.log("Guardando promocion")
     const { 
         promotion_name,
         promotion_description,
@@ -11,6 +10,7 @@ const savePromotion = async(req,res) => {
         promotion_value,
         promotion_products,
         product_id,
+        promotion_discount
 
      } = req.body
     const promotion_images = req.files
@@ -25,7 +25,7 @@ const savePromotion = async(req,res) => {
         INSERT INTO promotions(promotion_name, promotion_starts, promotion_ends, promotion_state, promotion_type) VALUES($1, $2, $3, $4, $5) RETURNING id
     `
     const query2 = `
-        INSERT INTO promotions_data(product_id, promotion_id, promotions_description, promotion_products_array) VALUES($1,$2, $3, $4)
+        INSERT INTO promotions_data(product_id, promotion_id, promotions_description, promotion_products_array, promotion_discount) VALUES($1,$2, $3, $4, $5)
     `
     const query3 = `
         INSERT INTO promotions_images(
@@ -49,11 +49,11 @@ const savePromotion = async(req,res) => {
         
 
         if(promotion_type === "single"){
-            const response2 = await client.query(query2,[product_id, promotion_id,promotion_description || "", null])
+            const response2 = await client.query(query2,[product_id, promotion_id,promotion_description || "", null, promotion_discount])
             if(response2.rowCount === 0) throw new Error("No se pudo registrar los datos de la promocion")
             console.log("Datos de la Promocion simple insertados con exito")
         }else{
-            const response2 = await client.query(query2, [null, promotion_id,promotion_description || "", promotion_products])  
+            const response2 = await client.query(query2, [null, promotion_id,promotion_description || "", promotion_products, promotion_discount])  
             if(response2.rowCount === 0) throw new Error("No se pudo registrar los datos de la promocion")
             console.log("Datos de la Promocion multiple insertados con exito")
         }
@@ -109,7 +109,6 @@ const getPromotions = async(req,res) => {
 
         if(response.rowCount === 0) return res.status(404).json({msg: "No hay promociones registradas"})
 
-         // 1️⃣ Mapeo de las promociones
          const promotions = response.rows.map(promotion => {
             let imageBase64Url = null;
             
@@ -134,6 +133,7 @@ const getPromotions = async(req,res) => {
                     promotion_ends: promotion.promotion_ends,
                     promotion_state: promotion.promotion_state,
                     promotion_type: promotion.promotion_type,
+                    promotion_discount: promotion.promotion_discount,
                     promotion_data: {
                         promotion_data_id: promotion.promotion_data_id,
                         promotion_description: promotion.promotions_description,
@@ -194,8 +194,107 @@ const deletePromotion = async(req,res) => {
     }
 }
 
+const editPromotion = async(req,res) => {
+    const { 
+        promotion_name,
+        promotion_description,
+        promotion_start_date,
+        promotion_end_date,
+        promotion_type,
+        promotion_value,
+        promotion_products,
+        product_id,
+        imagesWithEdit,
+        promotion_state,
+        promotion_discount
+     } = req.body
+     const { promotion_id } = req.params
+    const promotion_images = req.files
+
+    if(!promotion_name || promotion_images?.length === 0 || !promotion_type || !promotion_start_date || !promotion_end_date) return res.status(400).json({ msg: "Faltan datos importantes" })
+
+    if(promotion_type === "single" && !product_id) return res.status(400).json({ msg: "Faltan datos importantes" })
+    if(promotion_type === "multiple" && (!promotion_products || !promotion_value)) return res.status(400).json({ msg: "Faltan datos importantes" })
+    
+    let client;
+
+    try {
+        client = await pool.connect()
+        await client.query("BEGIN")
+        
+        const imagesList = await client.query(`SELECT image_name FROM promotions_images WHERE promotion_id = $1;`, [promotion_id]);
+        const existentImages = imagesList.rows.map(row => row.image_name);
+
+        console.log("Images Existentes en esta promo: ", existentImages.length)
+
+        const parsedImages = JSON.parse(imagesWithEdit);
+        const imagesToKeep = parsedImages.map(image => image.editing === true ? image.image_name : "").filter(image => image !== "");
+        console.log("Imagenes a Skipear: ", imagesToKeep.length)
+        const imagesToDelete = existentImages.filter(img => !imagesToKeep.includes(img))
+        console.log("Imagenes a borrar: ", imagesToDelete.length)
+        const newImages = promotion_images.filter(img => !parsedImages.some(i => i.image_name === img.originalname && i.editing))
+        console.log("Images nuevas: ", newImages.length)
+        if(imagesToDelete.length > 0) {
+            const resultDelete = await client.query(
+                `DELETE FROM promotions_images WHERE image_name = ANY($1) AND promotion_id = $2;`, 
+                [imagesToDelete, promotion_id]
+            );
+            
+            if(resultDelete.rowCount === 0) {
+                await client.query("ROLLBACK");
+                return res.status(400).json({ msg: "Ocurrio un error inesperado y no se pudo actualizar la promocion." });
+            }
+        }
+
+        if(newImages.length > 0) {
+            const insertQueries = promotion_images.map(image => client.query(`
+                INSERT INTO promotions_images (promotion_id, image_name, image_data, image_type, image_size) 
+                VALUES ($1, $2, $3, $4, $5);`, 
+                [promotion_id, image.originalname, image.buffer, image.mimetype, image.size]
+            ));
+            const resultInsertImage = await Promise.all(insertQueries);
+            const someInsertFalied = resultInsertImage.some(result => result.rowCount === 0);
+
+            if(someInsertFalied) {
+                await client.query("ROLLBACK");
+                return res.status(400).json({ msg: "Ocurrio un error inesperado y no se pudo actualizar la promocion." });
+            }
+        }
+
+        const stateOfPromotion = promotion_state === "true" ? true : false
+        console.log("Tipo de promoción: ",promotion_type)
+        const resultUpdate1 = await client.query(
+                `UPDATE promotions SET promotion_name = $1, promotion_starts = $2, promotion_ends = $3, promotion_state = $4, promotion_type = $5 WHERE id = $6;`,
+            [promotion_name, promotion_start_date, promotion_end_date, stateOfPromotion, promotion_type, promotion_id])
+        
+        if(resultUpdate1.rowCount === 0) {
+            throw new Error("Algo salio mal al editar la promocion, por favor, recargue esta seccion e intente nuevamente.")
+        }
+
+        const resultUpdate2 = await client.query(
+            `UPDATE promotions_data SET product_id = $1, promotions_description = $2, promotion_products_array = $3, promotion_discount = $4 WHERE promotion_id = $5;`,
+            [product_id || null, promotion_description, promotion_products || null, promotion_discount || 0, promotion_id]
+        )
+
+        if(resultUpdate2.rowCount === 0) {
+            throw new Error("Algo salio mal al editar la promocion, por favor, recargue esta seccion e intente nuevamente.")
+        }
+        
+        await client.query("COMMIT")
+        res.status(200).json({msg: "Promocion editada con exito"})
+    } catch (error) {
+        console.log(error)
+        await client.query("ROLLBACK")
+        res.status(500).json({msg: error.message || "Error interno del servidor al editar la promocion"})
+    }finally{
+        if(client) client.release()
+    }
+    
+}
+
 module.exports = { 
     savePromotion,
     getPromotions,
-    deletePromotion
+    deletePromotion,
+    editPromotion
  }
