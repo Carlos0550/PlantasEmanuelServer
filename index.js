0b1c16997949617a79310b92524ca0ea6db1131f
carlos0550 <carlospelinski03@gmail.com>
2024-12-17 10:34:35 -0300

    Implementa una tarea cron para actualizar el estado de las promociones

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const cron = require("node-cron")
+const dayjs = require("dayjs")
+const utc = require("dayjs/plugin/utc")
+const timezone = require("dayjs/plugin/timezone")
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
 require('dotenv').config(); 
 
 const app = express();
-const {checkConnection} = require('./config/database.js');
+const {checkConnection, pool} = require('./config/database.js');
 
 const adminsRoutes = require('./routes/admins.routes.js');
 const categoriesRoutes = require("./routes/categories.routes.js")
@@ -21,8 +28,38 @@ app.use("/api/categories", categoriesRoutes);
 app.use("/api/products", productsRoutes)
 app.use("/api/promotions", promotionsRouter);
 
+// setInterval(() => {
+//     const argentinaTime = dayjs().tz("America/Buenos_Aires")
+//     console.log(dayjs(argentinaTime).format("YYYY-MM-DD HH:mm:ss"));   
+// }, 1000);
 
-const PORT = process.env.PORT || 5000;
+cron.schedule("*/30 * * * *", async() => {
+    const argentinaTime = dayjs().tz("America/Buenos_Aires")
+    console.log(dayjs(argentinaTime).format("YYYY-MM-DD HH:mm:ss"));    
+    console.log("Ejecutando tarea programada");
+
+    const query1 = `UPDATE promotions SET promotion_state = true WHERE promotion_starts <= $1`
+    const query2 = `UPDATE promotions SET promotion_state = false WHERE promotion_ends <= $1`
+
+    let client;
+    try {
+        client = await pool.connect()
+
+        const response1 = await client.query(query1, [dayjs(argentinaTime).format("YYYY-MM-DD")])
+        const response2 = await client.query(query2, [dayjs(argentinaTime).format("YYYY-MM-DD")])
+        console.log("Filas actualizasdas", response1.rowCount, response2.rowCount)
+        if (response1.rowCount === 0 && response2.rowCount === 0) {
+            console.log("No se activó ni desactivó ninguna promoción.");
+        }
+    } catch (error) {
+        console.log(error)
+    }finally{
+        if(client) client.release()
+    }
+});
+
+
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
